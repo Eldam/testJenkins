c1b72ae3541d173d9981d8bb24a5f8ecafbe237c
eldam <mbmartin@esei.uvigo.es>
2018-04-25 17:28:19 +0200

    Solucion Suite

--- test/java/es/uvigo/esei/daa/suites/UnitTestSuite.java
+++ test/java/es/uvigo/esei/daa/suites/UnitTestSuite.java
@@ -4,9 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import es.uvigo.esei.daa.entities.EventUnitTest;
 
 @SuiteClasses({
-	
+	EventUnitTest.class
 })
 @RunWith(Suite.class)
 public class UnitTestSuite {
